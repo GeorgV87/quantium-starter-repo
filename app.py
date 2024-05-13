54f2522155ebf085937bf8ea70922d38b7fd6f85
Georg <georg.vietzke@gmail.com>
2024-05-13 13:46:33 +0200

    feat: improve and style line-chart

--- app.py
+++ app.py
@@ -1,29 +1,69 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, callback, Input, Output
 import plotly.express as px
 import pandas as pd
+import dash_bootstrap_components as dbc
 
 df = pd.read_csv("sales_pink_morsels.csv")
 df_sorted_by_date = df.sort_values("date")
 
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 
-
-app.layout = html.Div(
+title = dcc.Markdown(children="# Pink Morsel Sales")
+graph = dcc.Graph(figure={})
+radio = dcc.RadioItems(
     [
-        html.H1(children="Pink Morsel Sales", style={"textAlign": "center"}),
-        dcc.Graph(
-            figure=px.line(
-                df_sorted_by_date,
-                x="date",
-                y="sales",
-                color="region",
-                title="Impact on sales after price increase on the\
-                       15th of January, 2021",
-            )
-        ),
-    ]
+        {
+            "label": "all",
+            "value": "all",
+        },
+        {
+            "label": "north",
+            "value": "north",
+        },
+        {
+            "label": "east",
+            "value": "east",
+        },
+        {
+            "label": "south",
+            "value": "south",
+        },
+        {
+            "label": "west",
+            "value": "west",
+        },
+    ],
+    "all",
+    inline=True,
+    labelStyle={
+        "display": "inline",
+        "font-weight": "bold",
+        "font-size": 20,
+        "padding-right": 15,
+    },
+    inputStyle={"margin-right": 5},
 )
+
+app.layout = dbc.Container([title, graph, radio])
+
+
+@callback(
+    Output(graph, component_property="figure"), Input(radio, component_property="value")
+)
+def update_graph(user_input):
+    if user_input == "all":
+        fig = px.line(df_sorted_by_date, x="date", y="sales", title="Overall Sales")
+
+    else:
+        fig = px.line(
+            df_sorted_by_date.loc[df_sorted_by_date["region"].str.contains(user_input)],
+            x="date",
+            y="sales",
+            title=f"Sales Region {user_input}",
+        )
+
+    return fig
 
 
 if __name__ == "__main__":
